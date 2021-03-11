323a520d85205a8ce127d62bd697b6afa5e2dea3
Leah Brenny <leahbrenny@laniehei.localdomain>
2021-03-11 16:07:33 -0600

    added a details page

--- src/pet.js
+++ src/pet.js
@@ -1,27 +1,23 @@
-// const Pet = (props) => {
-//     return React.createElement("div", {}, [
-//       React.createElement("h1", {}, props.name),
-//       React.createElement("h2", {}, props.animal),
-//       React.createElement("h2", {}, props.breed),
-//     ]);
-//   };
+import { Link } from "react-router-dom";
 
-const Pet = ({ name, animal, breed, images, location, id }) => {
+const Pet = (props) => {
+  const { name, animal, breed, images, location, id } = props;
+
   let hero = "http://pets-images.dev-apis.com/pets/none.jpg";
   if (images.length) {
     hero = images[0];
   }
 
   return (
-    <a href={`/details/${id}`} className="pet">
+    <Link to={`/details/${id}`} className="pet">
       <div className="image-container">
         <img src={hero} alt={name} />
       </div>
       <div className="info">
         <h1>{name}</h1>
-        <h2>{`${animal} - ${breed} - ${location}`}</h2>
+        <h2>{`${animal} — ${breed} — ${location}`}</h2>
       </div>
-    </a>
+    </Link>
   );
 };
 
