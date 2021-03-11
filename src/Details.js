99fc0eed30e29f513bf98fecd5ca21812befbefb
Leah Brenny <leahbrenny@laniehei.localdomain>
2021-03-11 16:12:28 -0600

    added a loading... screen

--- src/Details.js
+++ src/Details.js
@@ -22,17 +22,21 @@ class Details extends Component {
     );
   }
   render() {
-      const { animal, breed, city, state, description, name} = this.state;
-    return(
-<div className="details">
-<div>
-    <h1>{name}</h1>
-    <h2>{`${animal} - ${breed} - ${city}, ${state}`}</h2>
-    <button>Adopt {name}</button>
-    <p>{description}</p>
-</div>
-</div>
-    )
+    if (this.state.loading) {
+      return <h2>loading...</h2>;
+    }
+
+    const { animal, breed, city, state, description, name } = this.state;
+    return (
+      <div className="details">
+        <div>
+          <h1>{name}</h1>
+          <h2>{`${animal} - ${breed} - ${city}, ${state}`}</h2>
+          <button>Adopt {name}</button>
+          <p>{description}</p>
+        </div>
+      </div>
+    );
   }
 }
 
